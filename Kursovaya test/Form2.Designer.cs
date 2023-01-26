2b859b8d0eb0501e28d7dd9e447a4930c78fc158
yahnodmitry <yahno.dmitry@gmail.com>
2023-01-26 09:26:59 +0200

    sorting updata

--- Kursovaya test/Form2.Designer.cs
+++ Kursovaya test/Form2.Designer.cs
@@ -50,12 +50,14 @@
             this.sort_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.sort_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.sort_button.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.sort_button.Location = new System.Drawing.Point(376, 161);
+            this.sort_button.Location = new System.Drawing.Point(501, 198);
+            this.sort_button.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sort_button.Name = "sort_button";
-            this.sort_button.Size = new System.Drawing.Size(146, 32);
+            this.sort_button.Size = new System.Drawing.Size(195, 39);
             this.sort_button.TabIndex = 1;
             this.sort_button.Text = "Сортування";
             this.sort_button.UseVisualStyleBackColor = false;
+            this.sort_button.Click += new System.EventHandler(this.sort_button_Click);
             // 
             // Comparison_btn
             // 
@@ -64,9 +66,10 @@
             this.Comparison_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.Comparison_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Comparison_btn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Comparison_btn.Location = new System.Drawing.Point(376, 363);
+            this.Comparison_btn.Location = new System.Drawing.Point(501, 447);
+            this.Comparison_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Comparison_btn.Name = "Comparison_btn";
-            this.Comparison_btn.Size = new System.Drawing.Size(125, 32);
+            this.Comparison_btn.Size = new System.Drawing.Size(167, 39);
             this.Comparison_btn.TabIndex = 4;
             this.Comparison_btn.Text = "Порівняння";
             this.Comparison_btn.UseVisualStyleBackColor = false;
@@ -75,18 +78,20 @@
             // Search
             // 
             this.Search.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Search.Location = new System.Drawing.Point(429, 78);
+            this.Search.Location = new System.Drawing.Point(572, 96);
+            this.Search.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Search.Name = "Search";
-            this.Search.Size = new System.Drawing.Size(147, 26);
+            this.Search.Size = new System.Drawing.Size(195, 30);
             this.Search.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(373, 81);
+            this.label1.Location = new System.Drawing.Point(497, 100);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 19);
+            this.label1.Size = new System.Drawing.Size(67, 22);
             this.label1.TabIndex = 6;
             this.label1.Text = "Пошук";
             // 
@@ -94,9 +99,10 @@
             // 
             this.Profit_sort.AutoSize = true;
             this.Profit_sort.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Profit_sort.Location = new System.Drawing.Point(376, 199);
+            this.Profit_sort.Location = new System.Drawing.Point(501, 245);
+            this.Profit_sort.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Profit_sort.Name = "Profit_sort";
-            this.Profit_sort.Size = new System.Drawing.Size(200, 23);
+            this.Profit_sort.Size = new System.Drawing.Size(249, 26);
             this.Profit_sort.TabIndex = 7;
             this.Profit_sort.TabStop = true;
             this.Profit_sort.Text = "Сортування за прибутком";
@@ -106,9 +112,10 @@
             // 
             this.Export_sort.AutoSize = true;
             this.Export_sort.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Export_sort.Location = new System.Drawing.Point(376, 222);
+            this.Export_sort.Location = new System.Drawing.Point(501, 273);
+            this.Export_sort.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Export_sort.Name = "Export_sort";
-            this.Export_sort.Size = new System.Drawing.Size(200, 23);
+            this.Export_sort.Size = new System.Drawing.Size(249, 26);
             this.Export_sort.TabIndex = 8;
             this.Export_sort.TabStop = true;
             this.Export_sort.Text = "Сортування за експортом";
@@ -117,9 +124,10 @@
             // panel1
             // 
             this.panel1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Location = new System.Drawing.Point(16, 50);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(317, 397);
+            this.panel1.Size = new System.Drawing.Size(423, 489);
             this.panel1.TabIndex = 9;
             // 
             // Back_btn
@@ -129,9 +137,10 @@
             this.Back_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.Back_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Back_btn.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Back_btn.Location = new System.Drawing.Point(12, 3);
+            this.Back_btn.Location = new System.Drawing.Point(16, 4);
+            this.Back_btn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Back_btn.Name = "Back_btn";
-            this.Back_btn.Size = new System.Drawing.Size(85, 32);
+            this.Back_btn.Size = new System.Drawing.Size(113, 39);
             this.Back_btn.TabIndex = 10;
             this.Back_btn.Text = "<< Назад";
             this.Back_btn.UseVisualStyleBackColor = false;
@@ -140,18 +149,19 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(591, 22);
+            this.label2.Location = new System.Drawing.Point(788, 27);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 11;
             this.label2.Text = "label2";
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.Back_btn);
             this.Controls.Add(this.panel1);
@@ -162,6 +172,7 @@
             this.Controls.Add(this.Comparison_btn);
             this.Controls.Add(this.sort_button);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
--- Kursovaya test/Form2.cs
+++ Kursovaya test/Form2.cs
@@ -33,229 +33,7 @@ namespace Kursovaya_test
         {
             DataOperating.countryName = this.state;
             DataOperating.jsonDeserialization(lst);
-            //Lst_struct lst_s = new Lst_struct();
-            //DoubleList<Yearly> lst_s = new DoubleList<Yearly>();
-            //Yearly year2021;
-            //year2021.year = 2021;
-            //year2021.value = 1000;
-            //year2021.income = 3000;
-            //year2021.exp = 15;
-
-            //Yearly year2020;
-            //year2020.year = 2020;
-            //year2020.value = 1500;
-            //year2020.income = 4000;
-            //year2020.exp = 10;
-
-            //Yearly year2019;
-            //year2019.year = 2019;
-            //year2019.value = 1200;
-            //year2019.income = 3500;
-            //year2019.exp = 20;
-
-            //lst_s.add(year2019);
-            //lst_s.add(year2020);
-            //lst_s.add(year2021);
-
-            //Solid ugol = new Solid(lst_s, "Гравій", 100000);
-
-            ////Lst_struct lst2 = new Lst_struct();
-            //DoubleList<Yearly> lst2 = new DoubleList<Yearly>();
-            //year2021.year = 2021;
-            //year2021.value = 2000;
-            //year2021.income = 4000;
-            //year2021.exp = 25;
-
-            //year2020.year = 2020;
-            //year2020.value = 2500;
-            //year2020.income = 5000;
-            //year2020.exp = 20;
-
-            //year2019.year = 2019;
-            //year2019.value = 2200;
-            //year2019.income = 4500;
-            //year2019.exp = 30;
-
-            //lst2.add(year2019);
-            //lst2.add(year2020);
-            //lst2.add(year2021);
-
-            //Liquid neft = new Liquid(lst2, "neft", 200000);
-
-            ////Lst_struct lst3 = new Lst_struct();
-            //DoubleList<Yearly> lst3 = new DoubleList<Yearly>();
-            //year2021.year = 2021;
-            //year2021.value = 3000;
-            //year2021.income = 5000;
-            //year2021.exp = 35;
-
-            //year2020.year = 2020;
-            //year2020.value = 3500;
-            //year2020.income = 6000;
-            //year2020.exp = 30;
-
-            //year2019.year = 2019;
-            //year2019.value = 3200;
-            //year2019.income = 5500;
-            //year2019.exp = 40;
-
-            //lst3.add(year2019);
-            //lst3.add(year2020);
-            //lst3.add(year2021);
-
-            //Gas gas = new Gas(lst3, "gas", 300000);
-
-            //lst.add(ugol);
-            //lst.add(neft);
-            //lst.add(gas);
-
-            //List<Mineral> test = new List<Mineral>();
-            //test.Add(ugol);
-            //test.Add(neft);
-            //test.Add(gas);
-
-            //string jsonTest = JsonConvert.SerializeObject(test, Formatting.Indented, new JsonSerializerSettings
-            //{
-            //    TypeNameHandling = TypeNameHandling.All
-            //});
-
             
-            //List<Mineral> newlist;
-            //using (StreamReader file = File.OpenText("../../movie.json"))
-            //{
-            //    JsonSerializer serializer = new JsonSerializer
-            //    {
-            //        TypeNameHandling = TypeNameHandling.Auto,
-            //    };
-            //    newlist = ( List < Mineral >) serializer.Deserialize(file, typeof(List<Mineral>));
-            //}
-
-            //FileStream stream = new FileStream("../../ukraineyearly.csv", FileMode.Open, FileAccess.Read);
-            //StreamReader reader = new StreamReader(stream);
-            //bool found = false;
-            //string data;
-            //do
-            //{
-            //    data = reader.ReadLine();
-            //    var cols = data.Split(',');
-            //    if (cols[0] == newlist[0].Name)
-            //    {
-            //        found = true;
-            //        break;
-            //    }
-            //} while (data != null);
-            //if(found)
-            //{
-            //    var cols = data.Split(',');
-            //    int year = int.Parse(cols[1]) - 1;
-            //    do
-            //    {
-
-            //        if (int.Parse(cols[1]) == year + 1)
-            //        {
-            //            Yearly nextYear = new Yearly
-            //            {
-            //                year = int.Parse(cols[1]),
-            //                value = double.Parse(cols[2]),
-            //                exp = double.Parse(cols[3]),
-            //                income = double.Parse(cols[4])
-                            
-            //            };
-            //            if (newlist[0].list == null)
-            //                newlist[0].list = new DoubleList<Yearly>();
-            //            newlist[0].list.add(nextYear);
-            //            year = int.Parse(cols[1]);
-            //        }
-            //        else if(int.Parse(cols[1]) > year + 1)
-            //        {
-            //            Yearly nextYear = new Yearly
-            //            {
-            //                year = year + 1,
-            //                value = 0,
-            //                income = 0,
-            //                exp = 0
-            //            };
-            //            year++;
-            //        }
-            //        else
-            //        {
-            //           //ошибка
-            //        }
-            //        data = reader.ReadLine();
-            //        if(data != null)
-            //            cols = data.Split(',');
-            //    } while (cols[0] == newlist[0].Name && data != null);
-            //}
-            //else
-            //{
-            //    //ошибка
-            //}
-            //reader.Close();
-            //stream.Close();
-
-
-            //string val = "200";
-            //string inc = "10";
-            //string ex = "35";
-
-            //int year1 = newlist[0].list.tail.data.year + 1;
-            //newlist[0].Value -= double.Parse(val);
-
-            //using (StreamWriter file = File.CreateText("../../movie.json"))
-            //{
-            //    JsonSerializer serializer = new JsonSerializer
-            //    {
-            //        TypeNameHandling = TypeNameHandling.All,
-            //        Formatting = Formatting.Indented
-            //    };
-            //    serializer.Serialize(file, newlist);
-            //}
-
-            //string half1 = "";
-            //string half2 = "";
-            //string currentline = "";
-            //FileStream stream1 = new FileStream("../../ukraineyearly.csv", FileMode.Open, FileAccess.Read);
-            //StreamReader reader1 = new StreamReader(stream1);
-            //currentline = "";
-            //string[] cols1 = new String[1];
-            //while ((cols1[0] != newlist[0].Name || int.Parse(cols1[1]) != newlist[0].list.tail.data.year) && currentline != null)
-            //{
-            //    currentline = reader1.ReadLine();
-            //    cols1 = currentline.Split(',');
-            //    half1 += currentline + '\n';
-            //}
-            //half1.Remove(half1.Length -2 , 1);
-            //half2 = reader1.ReadToEnd();
-            //half2.Remove(0, 1);
-            //half2.Remove(half2.Length - 2, 1);
-            //reader1.Close();
-            //stream1.Close();
-
-            //string newdata = newlist[0].Name + ',' +
-            //    year1.ToString() + ',' +
-            //    val + ',' +
-            //    ex + ',' +
-            //    inc;
-            //stream1 = new FileStream("../../ukraineyearly.csv", FileMode.Open, FileAccess.Write);
-            //StreamWriter writer = new StreamWriter(stream1);
-            ////string full = half1 + newdata;
-            ////if (half2 != "")
-            ////    full += half2;
-
-            //writer.Write(half1);
-            //writer.Write(newdata);
-            //if (half2 != "")
-            //    writer.Write('\n' + half2);
-            ////writer.Write(full);
-            //writer.Flush();
-            //writer.Close();
-            //stream1.Close();
-
-            //List<Mineral> newlist = new List<Mineral>();
-            //newlist = JsonConvert.DeserializeObject<List<Mineral>>(jsonTest, new JsonSerializerSettings
-            //{
-            //    TypeNameHandling = TypeNameHandling.Auto
-            //});
             //minerals_list.Items.Add(lst.find(0));
 
             //minerals_list.Items.Add(neft.Name);
@@ -285,17 +63,17 @@ namespace Kursovaya_test
             //this.Controls.Add(label3);
 
             //panel1.Controls.Add(label3);
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < lst.size; i++)
             {
-                
-                //Label l = new Label();
-                //l.Name= lst.find(i).data.Name;
-                //l.Text = lst.find(i).data.Name; 
-                //l.Location = new Point(10, i * 22);
-                //l.Size = new Size(40, 20);
-                //l.Click += label_Click;
-                //this.Controls.Add(panel1);
-                //panel1.Controls.Add(l);
+
+                Label l = new Label();
+                l.Name = lst.find(i).data.Name;
+                l.Text = lst.find(i).data.Name;
+                l.Location = new Point(10, i * 22);
+                l.Size = new Size(40, 20);
+                l.Click += label_Click;
+                this.Controls.Add(panel1);
+                panel1.Controls.Add(l);
 
             }
 
@@ -333,14 +111,6 @@ namespace Kursovaya_test
         //    Form3 form3 = new Form3
         //}
 
-        private void minerals_list_Click(object sender, EventArgs e)
-        { 
-            //Mineral  m = minerals_list.Items.;
-            //Form3 form3 = new Form3(m);
-            ////this.Hide();
-            //form3.Show();
-        }
-
         private void Comparison_btn_Click(object sender, EventArgs e)
         {
             Form4 form4 = new Form4(lst, this);
@@ -354,6 +124,23 @@ namespace Kursovaya_test
             Form1 form1 = new Form1();
             form1.Show();
 
+        }
+
+        private void sort_button_Click(object sender, EventArgs e)
+        {
+            BinaryHeap sorting = new BinaryHeap();
+            if(Profit_sort.Checked == true)
+            {
+                sorting.sortByIncome(lst);
+            }
+            else if(Export_sort.Checked == true)
+            {
+                sorting.sortByExp(lst);
+            }
+            else
+            {
+                //ошибка
+            }
         }
     }
 }
